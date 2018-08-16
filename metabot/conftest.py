9f4f641a2ffc4b33197540bfd7c1a483c2634b26
Daniel Reed <nmlorg@gmail.com>
2018-08-16 14:56:37 -0700

    Enable https://github.com/nmlorg/ntelebot/commit/f25b193fadb17d760f1a2e2cebfcdac677421306.

---
+++ metabot/conftest.py
@@ -0,0 +1,5 @@
+"""Test environment defaults."""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from ntelebot.conftest import _bot_mock  # pylint: disable=unused-import

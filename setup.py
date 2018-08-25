2be5844603952809cb4b97e4fc2f4bdc9bbef7ff
Daniel Reed <nmlorg@gmail.com>
2018-08-24 21:01:13 -0700

    Woops, I didn't realize this was a literal list of packages.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setuptools
 
 setuptools.setup(
     name='metabot',
-    version='0.0.1dev',
+    version='0.0.1.dev0',
     author='Daniel Reed',
     author_email='nmlorg@gmail.com',
     description='Modularized, multi-account bot.',
     url='https://github.com/nmlorg/metabot',
-    packages=['metabot'],
+    packages=setuptools.find_packages(include=('metabot', 'metabot.*')),
     install_requires=['ntelebot'])

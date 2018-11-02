380fd130f6753a63282e7c16f4f79b686ac81bcf
Daniel Reed <nmlorg@gmail.com>
2018-11-01 22:10:36 -0700

    Restore metabot.conftest._dont_mangle_callback_data.

--- metabot/conftest.py
+++ metabot/conftest.py
@@ -2,4 +2,11 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import pytest
+
 from ntelebot.conftest import _bot_mock  # pylint: disable=unused-import
+
+
+@pytest.fixture(autouse=True)
+def _dont_mangle_callback_data(monkeypatch):
+    monkeypatch.setattr('ntelebot.keyboardutil.fix', lambda keyboard, maxlen=0: None)

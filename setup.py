13e8b1afed8e0c932b236edab64eeb485b294eac
Daniel Reed <nmlorg@gmail.com>
2018-11-01 22:38:28 -0700

    Include README.md in the PyPI package metadata.

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ import setuptools
 
 setuptools.setup(
     name='metabot',
-    version='0.0.1',
+    version='0.0.1.1',
     author='Daniel Reed',
     author_email='nmlorg@gmail.com',
     description='Modularized, multi-account bot.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/nmlorg/metabot',
     packages=setuptools.find_packages(include=('metabot', 'metabot.*')),
     namespace_packages=setuptools.find_packages(include=('metabot', 'metabot.*')),

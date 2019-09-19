538c413a78224493469bc7733325f00d40e8fe89
Daniel Reed <nmlorg@gmail.com>
2019-09-18 17:46:25 -0700

    WIP: Very preliminary support for natural language.
    Initially, only queries of the form "When is ...?" are supported, and the match criteria is very simplistic.

---
+++ metabot/modules/natlang.py
@@ -0,0 +1,86 @@
+"""Process natural-language messages."""
+
+import logging
+import re
+
+import pytz
+
+from metabot.modules import events
+
+
+def moddispatch(ctx, msg, modconf):  # pylint: disable=missing-docstring,unused-argument
+    if ctx.type == 'message' and not ctx.command:
+        return process(ctx, msg)
+
+    return False
+
+
+PATTERNS = ('when is( the( next)?)? (?P<event>.+?)( (at|in)( the)? (?P<location>.+?))?',)
+
+
+def process(ctx, msg):
+    """Look for free-form text corresponding to a line in PATTERNS, and dispatch it."""
+
+    text = ' '.join(re.split(r'\W+', ctx.text.lower())).strip()
+
+    for pattern in PATTERNS:
+        ret = re.match(pattern + '$', text)
+        if not ret:
+            continue
+        fields = ret.groupdict()
+        logging.info('Parsed: %s', ', '.join('%s=%r' % (k, v) for k, v in sorted(fields.items())))
+        if fields.get('event'):
+            return process_event(ctx, msg, fields)
+
+    return False
+
+
+def process_event(ctx, msg, query):  # pylint: disable=too-many-branches,too-many-locals
+    """Process queries of the form "When is the next XXX?"."""
+
+    if ctx.chat['type'] == 'private':
+        conf = ctx.bot.config['issue37']['events']['users']['%s' % ctx.user['id']]
+    else:
+        conf = ctx.bot.config['issue37']['moderator']['%s' % ctx.chat['id']]
+    calcodes = conf.get('calendars', '').split()
+    timezone = pytz.timezone(conf.get('timezone', 'UTC'))
+
+    did_first = did_cal = did_summary = did_venue = False
+    summaries = set()
+    venues = set()
+    local_id = None
+    while not did_first or not did_cal or not did_summary or not did_venue:
+        _, event, nextev = ctx.bot.multibot.multical.get_event(local_id)
+        if not event:
+            break
+        summary = event['summary'].lower()
+        location = event['location'].lower()
+        if query['event'] in summary and (query.get('location') or '') in location:
+            venue = location.split(',', 1)[0]
+            reasons = set()
+            calcode = event['local_id'].split(':', 1)[0]
+            if calcode not in calcodes:
+                if not did_first:
+                    did_first = True
+                    reasons.add(query['event'] + ' I know about')
+            elif not did_first or not did_cal:
+                did_first = did_cal = True
+                reasons.add('%s on the %s calendar' %
+                            (query['event'], ctx.bot.multibot.calendars[calcode]['name']))
+            else:
+                if not did_summary and summary not in summaries:
+                    did_summary = True
+                    reasons.add(event['summary'])
+                if not did_venue and venue not in venues:
+                    did_venue = True
+                    reasons.add('%s at %s' % (query['event'], event['location'].split(',', 1)[0]))
+
+            if reasons:
+                msg.add('The next %s is:', ' / '.join(sorted(reasons)))
+                msg.add(events.format_event(ctx.bot, event, timezone, full=False))
+                summaries.add(summary)
+                venues.add(venue)
+
+        if not nextev:
+            break
+        local_id = nextev['local_id']
---
+++ metabot/modules/test_natlang.py
@@ -0,0 +1,93 @@
+"""Tests for metabot.modules.natlang."""
+
+import pytest
+
+from metabot.calendars import loader
+from metabot.modules import natlang
+
+
+@pytest.fixture
+def conversation(build_conversation):  # pylint: disable=missing-docstring
+    return build_conversation(natlang)
+
+
+# pylint: disable=line-too-long
+
+
+def test_simple(conversation, monkeypatch):  # pylint: disable=redefined-outer-name
+    """Verify the natlang module."""
+
+    assert conversation.message('When is the next sock hop?') == ''
+
+    monkeypatch.setattr('time.time', lambda: 1000.)
+
+    other_cal = loader.get('static:other_cal')
+    assert other_cal.calcode == '2a366983'
+    other_cal.events = {
+        '2a366983:alpha': {
+            'description': 'Alpha Description',
+            'end': 3000,
+            'local_id': '2a366983:alpha',
+            'location': 'Alpha Venue, Rest of Alpha Location',
+            'start': 2000,
+            'summary': 'Other Sock Hop',
+        },
+    }
+    conversation.multibot.multical.add('static:other_cal')
+    conversation.multibot.calendars['2a366983'] = {'name': 'Other Group'}
+
+    my_cal = loader.get('static:my_cal')
+    assert my_cal.calcode == 'fc2d5e05'
+    my_cal.events = {
+        'fc2d5e05:bravo': {
+            'description': '',
+            'end': 5000,
+            'local_id': 'fc2d5e05:bravo',
+            'location': 'My Venue, Rest of My Location',
+            'start': 4000,
+            'summary': 'My Sock Hop',
+        },
+        'fc2d5e05:charlie': {
+            'description': '',
+            'end': 7000,
+            'local_id': 'fc2d5e05:charlie',
+            'location': 'My Venue, Rest of My Location',
+            'start': 6000,
+            'summary': 'Special Sock Hop',
+        },
+        'fc2d5e05:delta': {
+            'description': '',
+            'end': 9000,
+            'local_id': 'fc2d5e05:delta',
+            'location': 'Alternate Venue, Rest of Special Location',
+            'start': 8000,
+            'summary': 'Alternate Sock Hop',
+        },
+    }
+    conversation.multibot.multical.add('static:my_cal')
+    conversation.multibot.calendars['fc2d5e05'] = {'name': 'My Group'}
+
+    conversation.bot.config['issue37']['events']['users']['1000']['calendars'] = 'fc2d5e05'
+
+    assert conversation.message('When is the next sock hop?') == """\
+[chat_id=1000 disable_web_page_preview=True parse_mode=HTML]
+The next sock hop I know about is:
+
+<b>Other Sock Hop</b>
+<a href="https://t.me/modulestestbot?start=L2V2ZW50cyAyYTM2Njk4MzphbHBoYSBVVEM">TODAY, Thu 1ˢᵗ, 12:33–12:50ᵃᵐ</a> @ <a href="https://maps.google.com/maps?q=Alpha+Venue%2C+Rest+of+Alpha+Location">Alpha Venue</a>
+
+The next sock hop on the My Group calendar is:
+
+<b>My Sock Hop</b>
+<a href="https://t.me/modulestestbot?start=L2V2ZW50cyBmYzJkNWUwNTpicmF2byBVVEM">TODAY, Thu 1ˢᵗ, 1:06–1:23ᵃᵐ</a> @ <a href="https://maps.google.com/maps?q=My+Venue%2C+Rest+of+My+Location">My Venue</a>
+
+The next Special Sock Hop is:
+
+<b>Special Sock Hop</b>
+<a href="https://t.me/modulestestbot?start=L2V2ZW50cyBmYzJkNWUwNTpjaGFybGllIFVUQw">TODAY, Thu 1ˢᵗ, 1:40–1:56ᵃᵐ</a> @ <a href="https://maps.google.com/maps?q=My+Venue%2C+Rest+of+My+Location">My Venue</a>
+
+The next sock hop at Alternate Venue is:
+
+<b>Alternate Sock Hop</b>
+<a href="https://t.me/modulestestbot?start=L2V2ZW50cyBmYzJkNWUwNTpkZWx0YSBVVEM">TODAY, Thu 1ˢᵗ, 2:13–2:30ᵃᵐ</a> @ <a href="https://maps.google.com/maps?q=Alternate+Venue%2C+Rest+of+Special+Location">Alternate Venue</a>
+"""

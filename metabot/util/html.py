0a44ce58f7ca5846d231684ac03f42a1b555b239
Daniel Reed <nmlorg@gmail.com>
2024-07-04 13:55:37 -0700

    Telegram supports nested tags (and a bunch of new tags) now.
    See tdlib/td@b1d1ea2 and tdlib/td@52651a2.
    
    Preparing for #94.

--- metabot/util/html.py
+++ metabot/util/html.py
@@ -11,52 +11,67 @@ def cgi_escape(text):  # pylint: disable=missing-docstring
 
 
 class _HTMLSanitizer(html.parser.HTMLParser):
-    __pieces = __strip = __current = None
+    __pieces = __strip = __stack = None
 
     def sanitize(self, text, strip=False):
         """Sanitize (or optionally entirely strip) HTML for use in Telegram."""
 
         self.__pieces = []
         self.__strip = strip
+        self.__stack = []
         self.feed(text)
         self.close()
+        self.handle_endtag(None)
         return ''.join(self.__pieces)
+
+    naked = {'b', 'del', 'em', 'i', 'ins', 'pre', 's', 'strike', 'strong', 'tg-spoiler', 'u'}
+    coupled = {
+        'a': 'href',
+        'blockquote': 'expandable',
+        'code': 'class',
+        'span': 'class',
+        'tg-emoji': 'emoji-id',
+    }
 
     def handle_starttag(self, tag, attrs):
         if tag in ('br', 'div', 'p'):
             self.__pieces.append('\n')
             return
-        if self.__strip or tag not in ('a', 'b', 'code', 'em', 'i', 'pre', 'strong'):
+        if self.__strip or tag not in self.naked and tag not in self.coupled:
             return
-        if self.__current:
-            self.__pieces.append('</%s>' % self.__current)
-        self.__current = tag
-        if tag == 'a':
+        self.__stack.append(tag)
+        if tag in self.coupled:
+            attr = self.coupled[tag]
             attrs = dict(attrs)
-            if attrs.get('href'):
-                self.__pieces.append('<a href="%s">' % cgi_escape(attrs['href']))
+            if attrs.get(attr):
+                value = cgi_escape(attrs[attr])
+                self.__pieces.append(f'<{tag} {attr}="{value}">')
+            elif attr in attrs:
+                self.__pieces.append(f'<{tag} {attr}>')
             else:
-                self.__pieces.append('<a>')
+                self.__pieces.append(f'<{tag}>')
         else:
-            self.__pieces.append('<%s>' % tag)
+            self.__pieces.append(f'<{tag}>')
 
     def handle_endtag(self, tag):
-        if tag == self.__current:
-            self.__pieces.append('</%s>' % tag)
-            self.__current = None
+        while self.__stack:
+            lasttag = self.__stack.pop()
+            self.__pieces.append(f'</{lasttag}>')
+            if lasttag == tag:
+                break
 
     def handle_data(self, data):
         self.__pieces.append(cgi_escape(data))
 
     def handle_entityref(self, name):  # pragma: no cover
         if name in ('lt', 'gt', 'amp', 'quot'):
-            self.__pieces.append('&%s;' % name)
+            self.__pieces.append(f'&{name};')
         else:
             codepoint = html.entities.name2codepoint.get(name)
             if codepoint:
                 self.__pieces.append(chr(codepoint))
             else:
-                self.__pieces.append('&amp;%s;' % name)
+                self.__pieces.append(f'&amp;{name};')
 
     def handle_charref(self, name):  # pragma: no cover
         name = name.lower()
@@ -67,7 +82,7 @@ class _HTMLSanitizer(html.parser.HTMLParser):
                 codepoint = int(name)
             self.__pieces.append(cgi_escape(chr(codepoint)))
         except (OverflowError, ValueError):
-            self.__pieces.append('&amp;#%s;' % name)
+            self.__pieces.append(f'&amp;#{name};')
 
     def error(self, message):  # pragma: no cover  pylint: disable=missing-function-docstring
         # Remove when support for Python below 3.10 is dropped.
--- metabot/util/test_html.py
+++ metabot/util/test_html.py
@@ -15,19 +15,27 @@ def test_sanitize():
     text = """
 <b>bold</b>, <strong>bold</strong>
 <i>italic</i>, <em>italic</em>
+<u>underline</u>, <ins>underline</ins>
+<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
+<span class="tg-spoiler">spoiler</span>, <tg-spoiler>spoiler</tg-spoiler>
+<b>bold <i>italic bold <s>italic bold strikethrough <span class="tg-spoiler">italic bold strikethrough spoiler</span></s> <u>underline italic bold</u></i> bold</b>
 <a href="http://www.example.com/">inline URL</a>
 <a href="tg://user?id=123456789">inline mention of a user</a>
+<tg-emoji emoji-id="5368324170671202286">👍</tg-emoji>
 <code>inline fixed-width code</code>
 <pre>pre-formatted fixed-width code block</pre>
+<pre><code class="language-python">pre-formatted fixed-width code block written in the Python programming language</code></pre>
+<blockquote>Block quotation started\nBlock quotation continued\nThe last line of the block quotation</blockquote>
+<blockquote expandable>Expandable block quotation started\nExpandable block quotation continued\nExpandable block quotation continued\nHidden by default part of the block quotation started\nExpandable block quotation continued\nThe last line of the block quotation</blockquote>
 """
     assert html.sanitize(text) == text
     assert html.sanitize('a<br>b<div>c</div><p>d</p>') == 'a\nb\nc\nd'
     assert html.sanitize('<a weird="true">text</a>') == '<a>text</a>'
 
-    assert html.sanitize('<b>good</b> <span>bad</span>') == '<b>good</b> bad'
-    assert html.sanitize('<b><span>nested</span></b>') == '<b>nested</b>'
-    assert html.sanitize('<span><b>nested</b></span>') == '<b>nested</b>'
-    assert html.sanitize('<b>broken <i>good</i>') == '<b>broken </b><i>good</i>'
+    assert html.sanitize('<b>good</b> <bogus>bad</bogus>') == '<b>good</b> bad'
+    assert html.sanitize('<b><bogus>nested</bogus></b>') == '<b>nested</b>'
+    assert html.sanitize('<bogus><b>nested</b></bogus>') == '<b>nested</b>'
+    assert html.sanitize('<b>broken <i>good</i>') == '<b>broken <i>good</i></b>'
 
     assert html.sanitize('&bogus;') == '&amp;bogus;'
     assert html.sanitize('&#bogus;') == '&amp;#bogus;'
@@ -46,18 +54,34 @@ def test_strip():
     text = """
 <b>bold</b>, <strong>bold</strong>
 <i>italic</i>, <em>italic</em>
+<u>underline</u>, <ins>underline</ins>
+<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
+<span class="tg-spoiler">spoiler</span>, <tg-spoiler>spoiler</tg-spoiler>
+<b>bold <i>italic bold <s>italic bold strikethrough <span class="tg-spoiler">italic bold strikethrough spoiler</span></s> <u>underline italic bold</u></i> bold</b>
 <a href="http://www.example.com/">inline URL</a>
 <a href="tg://user?id=123456789">inline mention of a user</a>
+<tg-emoji emoji-id="5368324170671202286">👍</tg-emoji>
 <code>inline fixed-width code</code>
 <pre>pre-formatted fixed-width code block</pre>
+<pre><code class="language-python">pre-formatted fixed-width code block written in the Python programming language</code></pre>
+<blockquote>Block quotation started\nBlock quotation continued\nThe last line of the block quotation</blockquote>
+<blockquote expandable>Expandable block quotation started\nExpandable block quotation continued\nExpandable block quotation continued\nHidden by default part of the block quotation started\nExpandable block quotation continued\nThe last line of the block quotation</blockquote>
 """
     assert html.sanitize(text, True) == """
 bold, bold
 italic, italic
+underline, underline
+strikethrough, strikethrough, strikethrough
+spoiler, spoiler
+bold italic bold italic bold strikethrough italic bold strikethrough spoiler underline italic bold bold
 inline URL
 inline mention of a user
+👍
 inline fixed-width code
 pre-formatted fixed-width code block
+pre-formatted fixed-width code block written in the Python programming language
+Block quotation started\nBlock quotation continued\nThe last line of the block quotation
+Expandable block quotation started\nExpandable block quotation continued\nExpandable block quotation continued\nHidden by default part of the block quotation started\nExpandable block quotation continued\nThe last line of the block quotation
 """
     assert html.sanitize('first<br>second', True) == 'first\nsecond'
     assert html.sanitize('<a weird="true">text</a>', True) == 'text'

a7a1c5b1fafa4899f17703a54178fb1c85598fc1
Daniel Reed <nmlorg@gmail.com>
2024-07-19 15:03:39 -0700

    Add an optional timezone argument to m.u.iso8601.totimestamp().
    See #104.
    
    Release as 0.3.24.

--- metabot/util/iso8601.py
+++ metabot/util/iso8601.py
@@ -3,11 +3,13 @@
 import datetime
 import re
 
+import pytz
 
-def totimestamp(isoformat):
+
+def totimestamp(isoformat, timezone=None):
     """Convert a string like YYYY-MM-DDTHH:MM:SS+HH:MM to the number of seconds since 1970-01-01."""
 
-    year, month, day, _, hour, minute, second, fraction, _, utchour, utcminute = re.match(
+    year, month, day, _, hour, minute, second, fraction, offset, utchour, utcminute = re.match(
         r'(\d{4})-?(\d{1,2})-?(\d{1,2})'
         r'(T(\d{1,2}):?(\d{1,2}):?(\d{1,2})'
         r'([.]\d+)?'
@@ -17,9 +19,13 @@ def totimestamp(isoformat):
                               int(second or 0), int(1000000 * float(fraction or 0)))
     if utchour and utcminute:
         tzinfo = _TimeZone(int(utchour) + float(utcminute) / 60)
+    elif offset == 'Z':
+        tzinfo = pytz.utc
+    elif timezone:
+        tzinfo = pytz.timezone(timezone)
     else:
-        tzinfo = _UTC
-    return (dtime.replace(tzinfo=tzinfo) - _EPOCH).total_seconds()
+        tzinfo = pytz.utc
+    return (tzinfo.localize(dtime) - _EPOCH).total_seconds()
 
 
 class _TimeZone(datetime.tzinfo):
@@ -40,6 +46,10 @@ class _TimeZone(datetime.tzinfo):
     def dst(unused_dt):  # pragma: no cover  pylint: disable=arguments-differ
         return datetime.timedelta(0)
 
+    def localize(self, dt):
+        """See https://pypi.org/project/pytz/#example-usage."""
 
-_UTC = _TimeZone(0)
-_EPOCH = datetime.datetime(1970, 1, 1, tzinfo=_UTC)
+        return dt.replace(tzinfo=self)
+
+
+_EPOCH = datetime.datetime(1970, 1, 1, tzinfo=pytz.utc)

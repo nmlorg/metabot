998bfd2bdfade0d8644b58f1cafb73f69c0fbde6
Daniel Reed <nmlorg@gmail.com>
2025-11-16 15:41:48 -0800

    Don't ever request weather information live, just keep track of addresses looked up over the past 2 days and continuously look them up in the background (spread evenly over a 9.5-minute period).

--- metabot/util/geoutil.py
+++ metabot/util/geoutil.py
@@ -1,6 +1,7 @@
 """Quick utilities for geocoding an address and looking up its weather forecast."""
 
 import logging
+import threading
 import time
 import urllib.parse
 
@@ -19,13 +20,55 @@ else:
 _CACHEFILE = 'config/geoutil.pickle'
 _CACHE = pickleutil.load(_CACHEFILE) or {}
 _SHORTCACHE = {}
+_SHORTCACHE_AGE = 60 * 9.5
+_PREFETCH_AGE = 60 * 60 * 24 * 2
+
+
+def _periodic():
+    while True:
+        request_cutoff = time.time() - _PREFETCH_AGE
+        candidates = sorted((last_check, url)
+                            for url, (last_request, last_check, forecast) in _SHORTCACHE.items()
+                            if last_request >= request_cutoff)
+        delay = 60
+        if candidates:
+            _cachedfetch(candidates[0][1], live=False)
+            delay = max(5, _SHORTCACHE_AGE / len(candidates))
+        time.sleep(delay)
+
+
+def _init():
+    thr = threading.Thread(target=_periodic)
+    thr.daemon = True
+    thr.start()
+
+
+_init()
+
+
+def _cachedfetch(url, *, live=True):
+    now = time.time()
+    last_request, last_check, forecast = _SHORTCACHE.get(url) or (0, 0, None)
+    if live:
+        last_request = now
+    elif last_check <= now - _SHORTCACHE_AGE:
+        try:
+            forecast = _weatherfetch(url)
+        except (ntelebot.requests.ConnectionError, ntelebot.requests.ReadTimeout):
+            logging.info('Timeout fetching %r.', url)
+        except Exception:  # pylint: disable=broad-except
+            logging.exception('While fetching %r:', url)
+        else:
+            last_check = now
+    _SHORTCACHE[url] = (last_request, last_check, forecast)
+    return forecast
 
 
 def _save():
     pickleutil.dump(_CACHEFILE, _CACHE)
 
 
-def geocode(address):
+def _geocode(address):
     """Look up the given address in Google Maps."""
 
     if _CLIENT is None:
@@ -58,36 +101,34 @@ def _weatherpoint(lat, lon):
     if 'weatherpoint' not in _CACHE:
         _CACHE['weatherpoint'] = {}
     if key not in _CACHE['weatherpoint']:
-        _CACHE['weatherpoint'][key] = _weatherfetch('points/' + key)
-        _save()
-    return _CACHE['weatherpoint'][key]
-
-
-def _hourlyforecast(lat, lon, when):
-    """Retrieve the hourly forecast for the given latitude/longitude."""
-
-    point = _weatherpoint(lat, lon)
-    if not point.get('properties'):
-        return
-    now = time.time()
-    url = point['properties']['forecastHourly']
-    last, ret = _SHORTCACHE.get(url) or (0, None)
-    if last < now - 10 * 60:
-        ret = _weatherfetch(url)['properties']['periods']
-        _SHORTCACHE[url] = (now, ret)
-
-    for period in ret:
-        start = iso8601.totimestamp(period['startTime'])
-        end = iso8601.totimestamp(period['endTime'])
-        if start <= when < end:
-            return period
+        try:
+            _CACHE['weatherpoint'][key] = _weatherfetch('points/' + key)
+        except (ntelebot.requests.ConnectionError, ntelebot.requests.ReadTimeout):
+            logging.exception('Timeout looking up point %r.', key)
+        except Exception:  # pylint: disable=broad-except
+            logging.exception('While looking up point %r:', key)
+        else:
+            _save()
+    return _CACHE['weatherpoint'].get(key)
 
 
 def hourlyforecast(address, when):
     """Retrieve the hourly forecast for the given address."""
 
-    geo = geocode(address)
-    if not geo:
+    if not (geo := _geocode(address)):
         return
-    return _hourlyforecast(geo[0]['geometry']['location']['lat'],
-                           geo[0]['geometry']['location']['lng'], when)
+    lat = geo[0]['geometry']['location']['lat']
+    lon = geo[0]['geometry']['location']['lng']
+
+    if not (point := _weatherpoint(lat, lon)):
+        return
+    url = point['properties']['forecastHourly']
+
+    if not (forecasts := _cachedfetch(url)):
+        return
+
+    for period in forecasts['properties']['periods']:
+        start = iso8601.totimestamp(period['startTime'])
+        end = iso8601.totimestamp(period['endTime'])
+        if start <= when < end:
+            return period
